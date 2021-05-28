db5052a5fbebc944a1654e2295117066177f3ccc
dre1597 <74803456+dre1597@users.noreply.github.com>
2021-05-28 02:57:57 -0300

    Add border-color and new goals on README

--- README.md
+++ README.md
@@ -1,16 +1,26 @@
-# Border Radius Previewer 
+# Border Radius Previewer
 
-## Border Radius Previwer with a result in CSS and a copy to clipboard 
+## About
+
+Border Radius Previewer with a result in CSS and a copy to clipboard
+
+In a future it's gonna be just a border previewer with no just radius, but color, width, style
 
 ## Technologies
-- HTML
-- Css
-- JavaScript
 
+-   HTML
+-   CSS
+-   JavaScript
 
-Still need some validations
+## Things to improve
+
+-   Responsive layout
+-   Still need more validations
+-   Add border style
+-   Add border width
+-   Build a on change preview, no just after click the button
+-   Add a background-color and color of the page too
 
 See more projects like this on [app-ideas](https://github.com/florinpop17/app-ideas)
 
 See this project [idea](https://github.com/florinpop17/app-ideas/blob/master/Projects/1-Beginner/Bin2Dec-App.md)
-
--- main.js
+++ main.js
@@ -1,50 +1,50 @@
-const div = document.querySelector('#div')
-const topLeft = document.querySelector('#top-left')
-const topRight = document.querySelector('#top-right')
-const bottomRight = document.querySelector('#bottom-right')
-const bottomLeft = document.querySelector('#bottom-left')
-const messageSpan = document.querySelector('#message')
-const generateBtn = document.querySelector('#generate')
-const cssText = document.querySelector('#css')
-const clipboardBtn = document.querySelector('#clipboard')
+const div = document.querySelector('#div');
+const topLeft = document.querySelector('#top-left');
+const topRight = document.querySelector('#top-right');
+const bottomRight = document.querySelector('#bottom-right');
+const bottomLeft = document.querySelector('#bottom-left');
+const messageSpan = document.querySelector('#message');
+const generateBtn = document.querySelector('#generate');
+const cssText = document.querySelector('#css');
+const clipboardBtn = document.querySelector('#clipboard');
 
 generateBtn.addEventListener('click', (event) => {
-    event.preventDefault()
+    event.preventDefault();
 
-    if(topLeft.value == '') {
-        div.style.borderTopLeftRadius = '0px'
-        topLeft.value = '0px'
-    }
-    else div.style.borderTopLeftRadius = topLeft.value
+    if (topLeft.value == '') {
+        div.style.borderTopLeftRadius = '0px';
+        topLeft.value = '0px';
+    } else div.style.borderTopLeftRadius = topLeft.value;
 
-    if(topRight.value == '') {
-        div.style.borderTopRightRadius = '0px'
-        topRight.value = '0px'
-    }
-    else div.style.borderTopRightRadius = topRight.value
-    
-    if(bottomRight.value == '') {
-        div.style.borderBottomRightRadius = '0px'
-        bottomRight.value = '0px'
-    }
-    else div.style.borderBottomRightRadius = bottomRight.value
+    if (topRight.value == '') {
+        div.style.borderTopRightRadius = '0px';
+        topRight.value = '0px';
+    } else div.style.borderTopRightRadius = topRight.value;
 
-    if(bottomLeft.value == '') {
-        div.style.borderBottomLeftRadius = '0px'
-        bottomLeft.value = '0px'
-    }
-    else div.style.borderBottomLeftRadius = bottomLeft.value
+    if (bottomRight.value == '') {
+        div.style.borderBottomRightRadius = '0px';
+        bottomRight.value = '0px';
+    } else div.style.borderBottomRightRadius = bottomRight.value;
 
-    cssText.textContent = `border: ${topLeft.value} ${topRight.value} ${bottomRight.value} ${bottomLeft.value};`
-    cssText.classList.remove('hidden')
-    clipboardBtn.classList.remove('hidden')
+    if (bottomLeft.value == '') {
+        div.style.borderBottomLeftRadius = '0px';
+        bottomLeft.value = '0px';
+    } else div.style.borderBottomLeftRadius = bottomLeft.value;
+
+    const color = document.querySelector('#color').value;
+    div.style.borderColor = color;
+
+    cssText.textContent = `border: ${topLeft.value} ${topRight.value} ${bottomRight.value} ${bottomLeft.value};
+    border-color: ${color};`;
+    cssText.classList.remove('hidden');
+    clipboardBtn.classList.remove('hidden');
 
     clipboardBtn.addEventListener('click', () => {
-        cssText.select()
-        cssText.setSelectionRange(0, 999999)
+        cssText.select();
+        cssText.setSelectionRange(0, 999999);
 
-        document.execCommand('copy')
+        document.execCommand('copy');
 
-        alert('Copied the text:' + cssText.value)
-    })
-})
+        alert('Copied the text:' + cssText.value);
+    });
+});
